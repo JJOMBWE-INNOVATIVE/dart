820eaf2eacfd5d55f9eb63cfe1e9fbca36e5d30d
JJOMBWE-INNOVATIVE <jjombwe8nathan@gmail.com>
2023-08-16 13:19:24 +0300

    Initial commit

---
+++ lib/assignment.dart
@@ -0,0 +1,15 @@
+void main(){
+  List houses = ["BangaloHouse","FlatHouse","GlassHouse","GreenHouse"];
+  List roads = [];
+  roads.add("KampalaRoad");
+  roads.add("JinjaRoad");
+  roads.add("MityanaRoad");
+  roads.add("HoimaRoad");
+
+List combinedList = houses + roads;
+
+print(combinedList);
+
+
+
+}
